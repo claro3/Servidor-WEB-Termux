7f13f8711071062a5e968e28f76be81f70381904
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2020-04-06 19:33:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,12 @@ echo'           __/ |
 echo'           |___/
 apt update && apt upgrade
 apt install php
+apt install apache2 
+pwd
+cd /data/data/com.termux/files/usr/share;ls
+cd apache2;ls
+cd default-site;ls
+cd htdocs;ls
+pwd
+cd --
+echo' Hola
